8d188b890caeeccc6fa87eb1df3668f58f648b9d
arshsyee <151017515+arshsyee@users.noreply.github.com>
2025-03-01 15:44:52 -0500

    Intial commit

---
+++ test.py
@@ -0,0 +1,19 @@
+import sys
+import numpy as np
+import pandas as pd
+import sklearn
+import nltk
+import spacy
+import yfinance as yf
+import requests
+import bs4
+
+print("Python executable in use:", sys.executable)
+print("Numpy version:", np.__version__)
+print("Pandas version:", pd.__version__)
+print("Scikit-learn version:", sklearn.__version__)
+print("NLTK version:", nltk.__version__)
+print("spaCy version:", spacy.__version__)
+print("yfinance version:", yf.__version__)
+print("Requests version:", requests.__version__)
+print("BeautifulSoup4 version:", bs4.__version__)
