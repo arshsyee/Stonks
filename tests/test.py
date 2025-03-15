e8ab7a09cf269add8bfbae6f67a2d77997b91c3e
arshsyee <151017515+arshsyee@users.noreply.github.com>
2025-03-15 02:39:04 -0400

    news api added

--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,46 @@
-import sys
-import numpy as np
-import pandas as pd
-import sklearn
-import nltk
-import spacy
-import yfinance as yf
 import requests
-import bs4
+from datetime import datetime
 
-print("Python executable in use:", sys.executable)
-print("Numpy version:", np.__version__)
-print("Pandas version:", pd.__version__)
-print("Scikit-learn version:", sklearn.__version__)
-print("NLTK version:", nltk.__version__)
-print("spaCy version:", spacy.__version__)
-print("yfinance version:", yf.__version__)
-print("Requests version:", requests.__version__)
-print("BeautifulSoup4 version:", bs4.__version__)
+def fetch_bbc_headlines(api_key):
+    url = "https://newsapi.org/v2/top-headlines"
+    params = {
+        #'sources': 'bbc-news',
+        'q':'bitcoin',
+        'pageSize':20,
+        'apiKey': api_key
+    }
+    
+    response = requests.get(url, params=params)     #checking if this piece of shit is working
+    data = response.json()
+    print(response)  # Debug print
+    
+    if data.get("status") != "ok":
+        print("Error:", data.get("message", "Unknown error"))
+        return
+
+    articles = data.get("articles", [])
+
+    if articles:
+        for i, article in enumerate(articles, start=1):
+            print(f"{i}. {article.get('title')}")
+            print(f"    URL link:{article.get('url','No url')}")
+            
+            source_dict = article.get('source', {})
+            source_name = source_dict.get('name', 'No source')
+            print(f"    Source: {source_name}")
+            
+            date_str = article.get('publishedAt')
+            if date_str:
+                date_str = date_str.replace('Z','+00:00')
+                published_dt = datetime.fromisoformat(date_str)
+                date = published_dt.strftime('%Y-%m-%d %H:%M:%S UTC')
+                print(f"    Published: {date}")
+            else:
+                print(f"    Date unavailable.")
+
+            print("---------")
+    else:
+        print("No headlines found.")
+
+if __name__ == "__main__":
+    fetch_bbc_headlines("915d9c51765c46bdae8c6089c0afac16")
